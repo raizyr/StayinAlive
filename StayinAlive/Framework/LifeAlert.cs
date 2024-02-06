71117e288d5f00995b117d4c46f2a36e79b497e6
Chris McNabb <raizyr@gmail.com>
2024-02-05 22:08:23 -0500

    Add project files.

---
+++ StayinAlive/Framework/LifeAlert.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using StardewModdingAPI.Events;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StayinAlive.Framework
+{
+    internal class LifeAlert
+    {
+        internal CueDefinition cue;
+
+
+        internal Mod Mod;
+        internal LifeAlert(Mod mod)
+        {
+            this.Mod = mod;
+            SoundEffect audio;
+            string filePathCombined = Path.Combine(this.Mod.Helper.DirectoryPath, "assets/low-health-warning.wav");
+            using (var stream = new System.IO.FileStream(filePathCombined, FileMode.Open))
+            {
+                audio = SoundEffect.FromStream(stream);
+            }
+
+            this.cue = new CueDefinition("low_health_warning", audio, Game1.audioEngine.GetCategoryIndex("Sound"), false)
+            {
+                instanceLimit = 1,
+                limitBehavior = CueDefinition.LimitBehavior.FailToPlay
+            };
+            Game1.soundBank.AddCue(this.cue);
+            this.Mod.Monitor.Log("Sound effects loaded", LogLevel.Debug);
+        }
+
+        internal void GameLoop_UpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (!Context.IsWorldReady) return;
+            var _maxHealth = Math.Max(Game1.player.health, Game1.player.maxHealth);
+            var CurrentHealthPercent = (float)Game1.player.health / _maxHealth;
+
+            if (CurrentHealthPercent < 0.5 && Game1.showingHealth)
+            {
+                Game1.playSound(cue.name);
+            }
+        }
+    }
+}
---
+++ StayinAlive/Framework/OverheadHealthbar.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+
+namespace StayinAlive.Framework
+{
+    internal sealed class OverheadHealthbar
+    {
+
+        private static Texture2D Pixel => _pixelLazy.Value;
+        private static Color BorderColor = new Color(255, 255, 255, 255);
+        private static float CurrentHealthPercent = 1; // Current player health percentage as decimal
+
+
+        internal static Texture2D Texture;
+        internal static int HeightAboveHead = 8;
+
+        internal Mod Mod;
+
+        public OverheadHealthbar(Mod mod)
+        {
+            this.Mod = mod;
+        }
+
+        private static void DrawTexture(Farmer player)
+        {
+            var _playerPos = player.getLocalPosition(Game1.viewport);
+
+            // Draw
+            Game1.spriteBatch.Draw(
+                Texture,
+                new Rectangle(
+                    // player.X - Texture.scaledWidth / 2 + FarmerSprite.scaledWidth / 2
+                    (int)_playerPos.X - Texture.Width * Game1.pixelZoom / 2 + player.FarmerSprite.SpriteWidth * Game1.pixelZoom / 2,
+                    // player.Y - FarmerSprite.scaledHeight - scaledHeightAbovePlayerHead
+                    (int)_playerPos.Y - player.FarmerSprite.SpriteHeight * Game1.pixelZoom - HeightAboveHead * Game1.pixelZoom,
+                    Texture.Width * Game1.pixelZoom,
+                    Texture.Height * Game1.pixelZoom),
+                BorderColor);
+
+        }
+
+        private static void DrawBar(Farmer player) 
+        {
+            var _playerPos = player.getLocalPosition(Game1.viewport);
+            var _healthRec = new Rectangle(
+                   (int)_playerPos.X - Texture.Width * Game1.pixelZoom / 2 + player.FarmerSprite.SpriteWidth * Game1.pixelZoom / 2 + 3 * Game1.pixelZoom,
+                   (int)_playerPos.Y - player.FarmerSprite.SpriteHeight * Game1.pixelZoom - (HeightAboveHead - 3) * Game1.pixelZoom,
+                   (int)((Pixel.Width * Game1.pixelZoom * Texture.Width - 6 * Game1.pixelZoom) * CurrentHealthPercent),
+                   Pixel.Height * Game1.pixelZoom * 6);
+
+            // Draw current health bar
+            Game1.spriteBatch.Draw(Pixel, _healthRec, new Color(226, 64, 64, 255));
+        }
+
+        private static void DrawBarBackground(Farmer player)
+        {
+            var _playerPos = player.getLocalPosition(Game1.viewport);
+            var _healthBgRec = new Rectangle(
+                   (int)_playerPos.X - Texture.Width * Game1.pixelZoom / 2 + player.FarmerSprite.SpriteWidth * Game1.pixelZoom / 2 + 3 * Game1.pixelZoom,
+                   (int)_playerPos.Y - player.FarmerSprite.SpriteHeight * Game1.pixelZoom - (HeightAboveHead - 3) * Game1.pixelZoom,
+                   Pixel.Width * Game1.pixelZoom * Texture.Width - 6 * Game1.pixelZoom,
+                   Pixel.Height * Game1.pixelZoom * 6);
+
+            // Draw health bar background
+            Game1.spriteBatch.Draw(Pixel, _healthBgRec, new Color(0, 0, 0, 135));
+        }
+
+
+        private static readonly Lazy<Texture2D> _pixelLazy = new(() =>
+        {
+            Texture2D pixel = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
+            return pixel;
+        });
+
+        internal static void Display_RenderedWorld(object sender, RenderedWorldEventArgs e)
+        {
+            if (!Context.IsWorldReady) return;
+            if (!Game1.showingHealth) return;
+
+            DrawBarBackground(Game1.player);
+            DrawBar(Game1.player);
+            DrawTexture(Game1.player);
+        }
+
+        
+    }
+}
+
